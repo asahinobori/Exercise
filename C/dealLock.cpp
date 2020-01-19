9c896fe653842944f9ca1ec1f4d1c3b98449993e
chenxusheng <chenxusheng@bigo.sg>
2020-01-19 11:10:12 +0800

    dead lock test

---
+++ C/dealLock.cpp
@@ -0,0 +1,42 @@
+// compile: g++ -o deadLock -pthread deadLock.cpp
+
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+long num = 0;
+std::mutex num_mutex1;
+std::mutex num_mutex2;
+
+const int loopTime 1000000
+
+void numplus() {
+    std::cout << "plus" << std::endl;
+    std::lock_guard<std::mutex> lock1(num_mutex1);
+    for (int i = 0; i < loopTime; i++) {
+        // std::cout << "+";
+        num++;
+    }
+    std::cout << "plus fin" << std::endl;
+    std::lock_guard<std::mutex> lock2(num_mutex2);
+}
+
+void numsub() {
+    std::cout << "sub" << std::endl;
+    std::lock_guard<std::mutex> lock2(num_mutex2);
+    for (int i = 0; i < loopTime; i++) {
+        // std::cout << "-";
+        num--;
+    }
+    std::cout << "sub fin" << std::endl;
+    std::lock_guard<std::mutex> lock1(num_mutex1);
+}
+
+int main() {
+    std::thread t1(numplus);
+    std::thread t2(numsub);
+    t1.join();
+    t2.join();
+    std::cout << num << std::endl;
+    return 0;
+}
